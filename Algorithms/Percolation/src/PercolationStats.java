e54ce3abdc44cd0ad5928dbd27a52eabb4d90fe8
ZackMamba <aleinwand2020@gmail.com>
2016-02-10 13:25:11 -0500

    added MC sim so i can review on my phone at work

--- Algorithms/Percolation/src/PercolationStats.java
+++ Algorithms/Percolation/src/PercolationStats.java
@@ -5,31 +5,60 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats
 {  
+    double[] results;
+    double[] meanArray;
+    double[] stddevArray;
+    double[] loArray;
+    double[] hiArray;
+    
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {   
+        int i = 0;
+        double[] meanArray = new double[T];
+        double[] stddevArray = new double[T];
+        double[] loArray = new double[T];
+        double[] hiArray = new double[T];
+        while(i < T){
+            Percolation perc = new Percolation(N);
+            int counter = 1;
+            while (!perc.percolates()){
+                int x = randOpener(gridSize);
+                int y = randOpener(gridSize);
+                if(!perc.isOpen(x, y)){
+                    perc.open(x, y);
+                    counter++;
+                }
+            }
+            meanArray[i] = perc.mean();
+            stddevArray[i] = perc.stddev();
+            loArray[i] = perc.confidenceLo();
+            hiArray[i] = perc.confidenceHi();
+            i++;
+            
+    }
+//     // sample mean of percolation threshold
+//    public double mean()  {
+//        return 0;
+//    }
+//    
+//    // sample standard deviation of percolation threshold
+//    public double stddev(){
+//        return 0;
+//    }
+//   // low  endpoint of 95% confidence interval
+//    public double confidenceLo(){
+//        return 0;
+//        
+//    }
+//       
+//        // high endpoint of 95% confidence interval
+//
+//    public double confidenceHi(){
+//        return 0;
+//        
+//    
    public static void main(String[] args)  // test client (optional)
    {
-       System.out.println("Fuck You");
-       Percolation perc_one = new Percolation(1);
-       System.out.println(perc_one.percolates() == false);
-       System.out.println(perc_one.isOpen(0,0) == false);
-       perc_one.open(0,0);
-       System.out.println(perc_one.percolates() == true);
-       System.out.println(perc_one.isOpen(0,0) == true);
-       Percolation perc_two = new Percolation(5);
-       perc_two.open(2,0);
-       System.out.println(perc_two.percolates() == false);
-       perc_two.open(2,1);
-       System.out.println(perc_two.percolates() == false);
-       perc_two.open(2,2);
-       System.out.println(perc_two.percolates() == false);
-       perc_two.open(2,3);
-       System.out.println(perc_two.percolates() == false);
-       perc_two.open(2,4);
-       System.out.println(perc_two.percolates() == true);
-   
-       System.out.println(perc_two.isFull(2,0) == true);
-       System.out.println(perc_two.isFull(2,1) == true);
-       System.out.println(perc_two.isFull(2,2) == true);
-       System.out.println(perc_two.isFull(2,3) == true);
-       System.out.println(perc_two.isFull(2,4) == true);
+
     } 
 }
